4b80f868e884ffcdb5001b454f97898108bced61
NatalyCI741 <chalaipuznataly@gmial.com>
2025-08-12 19:08:37 -0500

    sistema de gestión de libros en Laravel

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LibroController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+    });
+    Auth::routes();
+    Route::resource('libros', LibroController::class)->middleware('auth');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
